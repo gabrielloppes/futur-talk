5c5aa1bfd326fd7386c14a548511b1367073e21e
mpleandro <48145725+mpleandro@users.noreply.github.com>
2020-01-27 23:21:41 -0300

    Mudanças Formulário
    Alterações no formulário (linhas comentadas em 130 a 142).
    Adicão do script de enviar formulário.
    Ainda precisa adicionar uma mensagem de sucesso ao formulário

---
+++ futur-talks/js/script.js
@@ -0,0 +1,7 @@
+
+
+    function formSubmit() {
+        event.preventDefault();
+        document.getElementById("contato").style.display = "none"
+        document.getElementById("formhidden").style.display = "block"
+    }
